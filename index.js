94d6249829dbcaa25c33366e86966ef834599d6a
Manuel Jesus Jimenez Navarro <mrmj1996@gmail.com>
2018-01-24 19:26:11 +0100

    Ajedrez V1.0

---
+++ index.js
@@ -0,0 +1,30 @@
+/**
+ * Modules
+ */
+
+var express = require('express');
+var path = require('path');
+
+/**
+ * Variables initialization
+ */
+
+var app = express();
+var port = 8080;
+var path = path.join(__dirname,"/views/index.html");
+
+
+/**
+ * Server configuration
+ */
+
+app.use(express.static(__dirname+"/public"));
+
+app.get('/', function(request, response){
+    console.log(path);
+    response.sendFile(path);
+});
+
+app.listen(port, function(){
+    console.log("Listening port %d", port);
+});
